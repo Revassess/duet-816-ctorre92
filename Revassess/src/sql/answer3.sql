6bee5a98aef5e1c277538d0ce024a337ce2ea69c
ctorre92 <christian.torre.1@gmail.com>
2020-08-10 17:27:08 -0400

    new attempt

--- Revassess/src/sql/answer3.sql
+++ Revassess/src/sql/answer3.sql
@@ -1,1 +1,1 @@
-select * from app_user inner join study_set on app_user.user_id = study_set.owner_id where study_set.owner_id = 5;
+select * from app_user inner join study_set on app_user.user_id = study_set.owner_id where app_user.user_id = 5;
